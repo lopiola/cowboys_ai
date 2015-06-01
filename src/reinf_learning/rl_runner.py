c7e030ba5431f16f67c35002b73c4ef65ead3cff
Łukasz Opioła <lukasz.opiola@gmail.com>
2015-06-01 11:57:03 +0200

    run some reinf learning tets

--- src/reinf_learning/rl_runner.py
+++ src/reinf_learning/rl_runner.py
@@ -13,7 +13,7 @@ gamma = 0.86
 epsilon = 0.09
 
 # learning_rounds_list = [0, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]
-learning_rounds_list = [100000, 200000, 500000]
+learning_rounds_list = [500000]
 test_rounds = 500
 single_test_repeats = 30
 
